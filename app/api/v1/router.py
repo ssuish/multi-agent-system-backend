e395092827ea5b7ccd2c1c54218174832d2bbc13
ssuish <quidor.adrean@gmail.com>
2026-04-18 22:52:36 +0800

    added chat api to router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1 import profiles
+from app.api.v1 import profiles, chat
 
 api_v1_router = APIRouter(prefix="/api/v1")
+
 api_v1_router.include_router(profiles.router)
+api_v1_router.include_router(chat.router)
